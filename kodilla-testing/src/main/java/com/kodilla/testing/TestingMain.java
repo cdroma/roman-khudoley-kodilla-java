2d4e9a10cf00f092a34c07c7876584aef4341406
Roman Khudoley <romankhudoley@gmail.com>
2024-04-10 12:36:11 +0200

    6.2 Added calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator1 = new Calculator();
+
+        int result1 = calculator1.substractAFromB(15, 8);
+
+        if (result1==7) {
+            System.out.println("Test OK, \n15-8=" + result1);
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator2 = new Calculator();
+
+        int result2 = calculator2.sumAandB(7, 3);
+
+        if (result2==10) {
+            System.out.println("Test OK, \n7+3=" + result2);
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("End of test...");
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public static int substractAFromB(int a, int b) {
+        return a - b;
+    }
+
+    public int sumAandB(int a, int b){
+        return a + b;
+    }
+}
