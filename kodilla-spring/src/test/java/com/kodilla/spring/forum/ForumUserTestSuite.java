7a74673c3ac136f4aead834f4bd774ac5fb448f5
Roman Khudoley <romankhudoley@gmail.com>
2024-05-24 09:58:58 +0200

    13.1 IoC & DI, bean ForumUser & ForumUserTestSuite

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.forum;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ForumUserTestSuite {
+
+    @Autowired
+    private ForumUser forumUser;
+
+    @Test
+    @DisplayName("Getting username test")
+    void testGetUsername() {
+        // Given/When
+        String username = forumUser.getUsername();
+
+        // Then
+        assertEquals("John Smith", username);
+    }
+}
